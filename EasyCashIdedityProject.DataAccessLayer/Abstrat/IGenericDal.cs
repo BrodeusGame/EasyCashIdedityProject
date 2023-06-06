a2ea1470e5e4a8c725c16aea998647c6fc2ee2fa
FATİH ÇOBAN <102518183+BrodeusGame@users.noreply.github.com>
2023-06-06 13:30:27 +0300

    ders 8 sonu

---
+++ EasyCashIdedityProject.DataAccessLayer/Abstrat/IGenericDal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.DataAccessLayer.Abstrat
+{
+	public interface IGenericDal<T> where T : class
+	{
+		void Insert(T entity);
+		void Update(T entity);
+		void Delete(T entity);
+		T GetByID(T entity);
+		List<T> GetList();
+
+
+	}
+}
---
+++ EasyCashIdedityProject.DataAccessLayer/Repositories/GanericRepository.cs
@@ -0,0 +1,47 @@
+﻿using EasyCashIdentityProject.DataAccessLayer.Abstrat;
+using EasyCashIdentityProject.DataAccessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.DataAccessLayer.Repositories
+{
+	public class GanericRepository<T> : IGenericDal<T> where T : class
+	{
+		public void Delete(T entity)
+		{
+			using var context = new Context();
+			context.Set<T>().Remove(entity);
+			context.SaveChanges();
+		}
+
+		public T GetByID(T id)
+		{
+			using var context =new Context();	
+			return context.Set<T>().Find(id);
+		}
+
+		public List<T> GetList()
+		{
+			using var context=new Context();
+			return context.Set<T>().ToList();
+
+		}
+
+		public void Insert(T entity)
+		{
+			using var context = new Context();
+			context.Set<T>().Add(entity);
+			context.SaveChanges();
+		}
+
+		public void Update(T entity)
+		{
+			using var context =new Context();
+			context.Set<T>().Update(entity);
+			context.SaveChanges();
+		}
+	}
+}
